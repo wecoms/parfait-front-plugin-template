a3b4d3baf9a067037e200bbbf46f8116e02f28e2
wecomsadmin <admin@wecoms.com>
2018-09-18 07:50:20 +0900

    Add files via upload

---
+++ template/src/front/store/hello-plugin.js
@@ -0,0 +1,16 @@
+export default {
+    namespaced: true,
+
+    state: {
+        users: null
+    },
+
+    mutations: {
+    },
+
+    actions: {
+        testUsers(context) {
+            console.log("test users");
+        }
+    }
+};
