fa640c6cb9eb0aee77e63b14e1cfc535878a8281
wecomsadmin <admin@wecoms.com>
2018-09-18 07:46:51 +0900

    Update index.js

--- template/src/front/index.js
+++ template/src/front/index.js
@@ -1,10 +1,23 @@
 import Vue from "vue"
-import HelloPlugin from "./components/HelloPlugin"
+import Vuex from "vuex"
+import Index from "./Index.vue"
+import RegisterStores from "./store/register-stores.js"
+
+import ParfaitFront from "parfait-front"
+ParfaitFront.use("w2ui");
+
+Vue.use(Vuex);
+const store = new Vuex.Store();
 
 new Vue({
     el: "#plugin",
+    template: "<index/>",
     components: {
-        HelloPlugin         // Add Your Components
+        Index
     },
-    template: "<hello-plugin/>"
+    store,
+    mounted() {
+        ParfaitContext.setMainComponent(this);
+        RegisterStores.register();
+    }
 });
