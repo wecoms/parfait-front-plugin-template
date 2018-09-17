5a319de1371791399c1c25ecdeaccc7ef8b5390c
wecomsadmin <admin@wecoms.com>
2018-09-18 07:44:32 +0900

    Add files via upload

---
+++ template/src/front/Index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <menu-management/>
+        <!--<user-management/>-->
+        <!--<notice/>-->
+        <!--<auth-management/>-->
+    </div>
+</template>
+
+<script>
+    import MenuManagement from "./components/menu/MenuManagement"
+    import UserManagement from "./components/user/UserManagement"
+    import Notice from "./components/notice/Notice"
+    import AuthManagement from "./components/auth/AuthManagement"
+
+    export default {
+        name: "Index",
+        components: {
+            MenuManagement, UserManagement, Notice, AuthManagement
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
