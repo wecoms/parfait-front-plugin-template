ebfbb546e71b0ae47752326e4b88d77659350320
wecomsadmin <admin@wecoms.com>
2018-10-10 06:10:50 +0900

    Update register-stores.js

--- template/src/front/store/register-stores.js
+++ template/src/front/store/register-stores.js
@@ -3,5 +3,12 @@ import helloPlugin from "./hello-plugin"
 export default {
     register: function() {
         ParfaitContext.getMainComponent().$store.registerModule("helloPlugin", helloPlugin);
+    },
+
+    load: function() {
+        return new Promise(function (resolve, reject) {
+            // initializing something..
+            resolve();
+        })
     }
 }
