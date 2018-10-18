888a4c0ddcc89b1120802e071672c6715e3a2d09
wecomsadmin <admin@wecoms.com>
2018-10-18 17:02:24 +0900

    Update plugin.js

--- template/src/front/plugin.js
+++ template/src/front/plugin.js
@@ -1,40 +1,8 @@
-import RegisterStores from "./store/register-stores.js"
-import HelloPlugin from "./components/HelloPlugin"
-
-const pluginRegisterStoreEntry = new ParfaitPluginEntry({
-    type: ParfaitPluginEntryType.RegisterStores,
-    target: function() {
-        RegisterStores.register();
-    }
-});
-
-const pluginMainLoadCompleteEntry = new ParfaitPluginEntry({
-    type: ParfaitPluginEntryType.MainLoadCompleteEvent,
-    target: function() {
-        return RegisterStores.load();
-    }
-});
-
-const pluginEntry = new ParfaitPluginEntry({
-    type: ParfaitPluginEntryType.MainMenuItem,
-    target: new ParfaitPluginEntryMainMenuItemTarget({
-        id: "hello-plugin-menu",
-        order: 0,
-        title: "메뉴1",
-        actionType: ParfaitPluginEntryMainMenuActionType.Injection,
-        actionComponent: HelloPlugin,
-        actionOptions: {
-            id: "hello-plugin",
-            caption: "메뉴1",
-            lockable: true,
-            closable: true
-        }
-    })
-});
+import PluginEntries from "./plugin-entries"
 
 const packageConfig = require('../../package.json');
 ParfaitPluginManager.getInstance().addPlugin(new ParfaitPlugin({
     id: packageConfig.name,
     version: packageConfig.version,
-    entries: [pluginRegisterStoreEntry, pluginMainLoadCompleteEntry, pluginEntry]
+    entries: PluginEntries.get()
 }));
