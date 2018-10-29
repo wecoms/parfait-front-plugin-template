12dbd2aa6e22e80df5a711fda0ad84835b32f8d0
wecomsadmin <admin@wecoms.com>
2018-10-29 23:07:47 +0900

    Update webpack.base.conf.js

--- template/build/webpack.base.conf.js
+++ template/build/webpack.base.conf.js
@@ -42,24 +42,27 @@ module.exports = {
                 test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
                 loader: 'url-loader',
                 options: {
-                    limit: 10000,
-                    name: utils.assetsPath('img/[name].[hash:7].[ext]')
+                    limit: 1,
+                    name: utils.assetsPath('images/[name].[ext]'),
+                    publicPath: utils.resourcesPublicPath()
                 }
             },
             {
                 test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
                 loader: 'url-loader',
                 options: {
-                    limit: 10000,
-                    name: utils.assetsPath('media/[name].[hash:7].[ext]')
+                    limit: 1,
+                    name: utils.assetsPath('media/[name].[ext]'),
+                    publicPath: utils.resourcesPublicPath()
                 }
             },
             {
                 test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
                 loader: 'url-loader',
                 options: {
-                    limit: 10000,
-                    name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+                    limit: 1,
+                    name: utils.assetsPath('fonts/[name].[ext]'),
+                    publicPath: utils.resourcesPublicPath()
                 }
             }
         ]
