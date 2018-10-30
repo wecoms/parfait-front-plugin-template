e7dbd849fc4993230b9bbb26fc887f78fc91a94e
wecomsadmin <admin@wecoms.com>
2018-10-30 18:55:38 +0900

    Update plugin.js

--- template/src/front/plugin.js
+++ template/src/front/plugin.js
@@ -1,8 +1,10 @@
 import PluginEntries from "./plugin-entries"
+import PluginComponents from "./plugin-components"
 
 const packageConfig = require('../../package.json');
 ParfaitPluginManager.getInstance().addPlugin(new ParfaitPlugin({
     id: packageConfig.name,
     version: packageConfig.version,
-    entries: PluginEntries.get()
+    entries: PluginEntries.get(),
+    components: PluginComponents.get()
 }));
