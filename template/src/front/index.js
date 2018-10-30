6a1ad2923f697cba55d21d24a2fc49859bc32f75
wecomsadmin <admin@wecoms.com>
2018-10-30 18:54:06 +0900

    Update index.js

--- template/src/front/index.js
+++ template/src/front/index.js
@@ -1,13 +1,13 @@
 import Vue from "vue"
 import Vuex from "vuex"
-import Index from "./Index.vue"
-import RegisterStores from "./store/register-stores.js"
-
 import ParfaitFront from "parfait-front"
 ParfaitFront.use("w2ui");
 
 Vue.use(Vuex);
 const store = new Vuex.Store();
+
+import Index from "./Index.vue"
+import RegisterStores from "./store/register-stores.js"
 
 new Vue({
     el: "#plugin",
@@ -18,10 +18,15 @@ new Vue({
     store,
     mounted() {
         ParfaitContext.setMainComponent(this);
-        RegisterStores.register();
-        RegisterStores.load()
-            .then(function() {
+        
+        ParfaitPluginUtil.loadsForDevelopment(
+            [
+                RegisterStores.register(),
+                RegisterStores.load()
+            ],
+            function() {
                 this.index = Index;
-            }.bind(this));
+            }.bind(this)
+        );
     }
 });
