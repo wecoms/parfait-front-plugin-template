043f3367be5d1444650a2ecd42b0583e17d52726
wecomsadmin <admin@wecoms.com>
2018-10-30 18:55:03 +0900

    Update plugin-components.js

--- template/src/front/plugin-components.js
+++ template/src/front/plugin-components.js
@@ -1,16 +1,8 @@
 // This file is created automatically by system.
 
-import LoginContent from "./components/login/LoginContent.vue"
-import NoticeList from "./components/notice/NoticeList.vue"
-import NoticeListDetail from "./components/notice/NoticeListDetail.vue"
-
 export default {
     get: function() {
         const map = new Map();
-        map.set(LoginContent.name, LoginContent);
-        map.set(NoticeList.name, NoticeList);
-        map.set(NoticeListDetail.name, NoticeListDetail);
-        
         return map;
     }
 }
