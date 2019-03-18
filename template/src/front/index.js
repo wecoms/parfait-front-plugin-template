78bf8e792bd62285a91a356701b46fc95e858329
wecomsadmin <admin@wecoms.com>
2019-03-18 17:05:15 +0900

    Update index.js

--- template/src/front/index.js
+++ template/src/front/index.js
@@ -1,15 +1,18 @@
 import Vue from "vue"
 import Vuex from "vuex"
+import VueRouter from "vue-router"
 import ParfaitFront from "parfait-front"
+import ParfaitKernel from "parfait-kernel"
 
 Vue.use(Vuex);
+Vue.use(VueRouter);
 Vue.use(ParfaitFront);
+Vue.use(ParfaitKernel);
 
-import "./plugin"
-import "./resources/css/common.css"
+import Main from "./main"
 import Index from "./Index.vue"
-import RegisterStores from "./store/register-stores.js"
 
+const router = new VueRouter();
 const store = new Vuex.Store();
 new Vue({
     el: "#plugin",
@@ -17,22 +20,27 @@ new Vue({
     data: {
         index: null
     },
+    router,
     store,
     mounted() {
         ParfaitContext.setMainComponent(this);
-        
+
         ParfaitPluginUtil.registerStores(
             [
-                RegisterStores.register
+                ParfaitKernel.registerStore,
+                Main.registerStore
             ]
         );
         ParfaitPluginUtil.loads(
             [
-                RegisterStores.load
+                ParfaitKernel.load,
+                Main.load
             ],
             function() {
                 this.index = Index;
             }.bind(this)
         );
+
+        this.index = Index;
     }
 });
