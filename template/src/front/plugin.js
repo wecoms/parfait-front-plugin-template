933a2fdefcb5185516bd7739293954e42e69ecf0
wecomsadmin <admin@wecoms.com>
2020-04-13 19:00:15 +0900

    Update plugin.js

--- template/src/front/plugin.js
+++ template/src/front/plugin.js
@@ -1,10 +1,12 @@
 import PluginEntries from "./plugin-entries"
 import PluginComponents from "./plugin-components"
+import PluginLocaleMessages from "./locale/messages"
 
 const packageConfig = require('../../package.json');
 ParfaitPluginManager.getInstance().addPlugin(new ParfaitPlugin({
     id: packageConfig.name,
     version: packageConfig.version,
     entries: PluginEntries.get(),
-    components: PluginComponents.get()
+    components: PluginComponents.get(),
+    localeMessages: PluginLocaleMessages
 }));
