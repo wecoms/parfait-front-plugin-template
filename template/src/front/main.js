5388e5376d3b5efd7fabd257651f7a2315c554fd
wecomsadmin <admin@wecoms.com>
2020-04-13 18:59:45 +0900

    Update main.js

--- template/src/front/main.js
+++ template/src/front/main.js
@@ -1,6 +1,7 @@
 import "./plugin"
 import "./resources/css/common.css"
 import RegisterStores from "./store/register-stores"
+import LoadMessages from "./locale/load-messages"
 
 export default {
     registerStore: function() {
@@ -9,5 +10,6 @@ export default {
 
     load: function() {
         RegisterStores.load();
+        LoadMessages.load();
     }
 }
