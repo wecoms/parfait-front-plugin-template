4b124258a75f9e9fb2732ce3c3f5ade39ee05da0
wecomsadmin <admin@wecoms.com>
2020-04-13 18:59:10 +0900

    Update index.js

--- template/src/front/index.js
+++ template/src/front/index.js
@@ -1,10 +1,12 @@
 import Vue from "vue"
 import Vuex from "vuex"
+import VueI18n from "vue-i18n"
 import VueRouter from "vue-router"
 import ParfaitFront from "parfait-front"
 import ParfaitKernel from "parfait-kernel"
 
 Vue.use(Vuex);
+Vue.use(VueI18n);
 Vue.use(VueRouter);
 Vue.use(ParfaitFront);
 Vue.use(ParfaitKernel);
@@ -14,12 +16,14 @@ import Index from "./Index.vue"
 
 const router = new VueRouter();
 const store = new Vuex.Store();
+const i18n = new VueI18n({ locale: "ko" });
 new Vue({
     el: "#plugin",
     template: "<div style='width: 100%; height: 100%;'><component :is='index'></component><vue-snotify/></div>",
     data: {
         index: null
     },
+    i18n,
     router,
     store,
     mounted() {
